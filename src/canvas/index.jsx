701ca7c98c95275706d8864361e52ebfd7cb4afa
Omar Khalifa <elkhalifa002@gmail.com>
2023-07-18 14:58:14 +0300

    remove Backdrop component

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -2,7 +2,6 @@ import { Canvas } from '@react-three/fiber'
 import { Environment, Center } from '@react-three/drei';
 
 import Shirt from './Shirt';
-import Backdrop from './Backdrop';
 import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
@@ -17,7 +16,6 @@ const CanvasModel = () => {
       {/* <Environment preset="city" /> */}
 
       <CameraRig>
-        <Backdrop />
         <Center>
           <Shirt />
         </Center>
