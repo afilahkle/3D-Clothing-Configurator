f61c3fd385c9c32a8882584d979a663cd14da75f
Omar Khalifa <elkhalifa002@gmail.com>
2023-07-18 14:58:51 +0300

    add orbitcontrols

--- src/canvas/Shirt.jsx
+++ src/canvas/Shirt.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { easing } from 'maath';
 import { useSnapshot } from 'valtio';
 import { useFrame } from '@react-three/fiber';
-import { Decal, useGLTF, useTexture } from '@react-three/drei';
+import { Decal, useGLTF, useTexture, OrbitControls } from '@react-three/drei';
 
 import state from '../store';
 
@@ -18,36 +18,39 @@ const Shirt = () => {
   const stateString = JSON.stringify(snap);
 
   return (
-    <group key={stateString}>
-      <mesh
-        castShadow
-        geometry={nodes.T_Shirt_male.geometry}
-        material={materials.lambert1}
-        material-roughness={1}
-        dispose={null}
-      >
-        {snap.isFullTexture && (
-          <Decal 
-            position={[0, 0, 0]}
-            rotation={[0, 0, 0]}
-            scale={1}
-            map={fullTexture}
-          />
-        )}
+    <>
+      <OrbitControls />
+      <group key={stateString}>
+        <mesh
+          castShadow
+          geometry={nodes.T_Shirt_male.geometry}
+          material={materials.lambert1}
+          material-roughness={1}
+          dispose={null}
+        >
+          {snap.isFullTexture && (
+            <Decal 
+              position={[0, 0, 0]}
+              rotation={[0, 0, 0]}
+              scale={1}
+              map={fullTexture}
+            />
+          )}
 
-        {snap.isLogoTexture && (
-          <Decal 
-            position={[0, 0.04, 0.15]}
-            rotation={[0, 0, 0]}
-            scale={0.15}
-            map={logoTexture}
-            map-anisotropy={16}
-            depthTest={false}
-            depthWrite={true}
-          />
-        )}
-      </mesh>
-    </group>
+          {snap.isLogoTexture && (
+            <Decal 
+              position={snap.logoPosition}
+              rotation={[0, 0, 0]}
+              scale={snap.logoScale}
+              map={logoTexture}
+              map-anisotropy={16}
+              depthTest={false}
+              depthWrite={true}
+            />
+          )}
+        </mesh>
+      </group>
+    </>
   )
 }
 
