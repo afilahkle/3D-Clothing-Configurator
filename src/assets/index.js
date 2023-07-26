d15c70679f7c080c84078eb5e7d2e8e7d1b192cb
Omar Khalifa <elkhalifa002@gmail.com>
2023-07-26 18:50:06 +0300

    add some textures and logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,5 +5,14 @@ import download from "./download.png";
 import logoShirt from "./logo-tshirt.png";
 import stylishShirt from "./stylish-tshirt.png";
 import logoControls from "./logo-controls.png";
+import textureLogoPicker from "./textureLogoPicker.jpeg";
+import texture1 from "./texture1.jpeg";
+import texture2 from "./texture2.jpeg";
+import texture3 from "./texture3.jpeg";
+import texture4 from "./texture4.jpeg";
+import texture5 from "./texture5.jpeg";
+import logo1 from "./logo1.jpeg";
+import logo2 from "./logo2.jpeg";
 
-export { fileIcon, swatch, download, logoShirt, stylishShirt, logoControls };
+
+export { fileIcon, swatch, download, logoShirt, stylishShirt, logoControls, textureLogoPicker, texture1, texture2, texture3, texture4, texture5, logo1, logo2 };
