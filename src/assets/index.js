33e5ac9a7d680d4802272a5f3215aea81fa8807a
Omar Khalifa <elkhalifa002@gmail.com>
2023-07-26 19:21:44 +0300

    remove background for logos

--- src/assets/index.js
+++ src/assets/index.js
@@ -11,8 +11,8 @@ import texture2 from "./texture2.jpeg";
 import texture3 from "./texture3.jpeg";
 import texture4 from "./texture4.jpeg";
 import texture5 from "./texture5.jpeg";
-import logo1 from "./logo1.jpeg";
-import logo2 from "./logo2.jpeg";
+import logo1 from "./logo1.png";
+import logo2 from "./logo2.png";
 
 
 export { fileIcon, swatch, download, logoShirt, stylishShirt, logoControls, textureLogoPicker, texture1, texture2, texture3, texture4, texture5, logo1, logo2 };
