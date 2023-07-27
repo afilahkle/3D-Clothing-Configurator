2c94d1127cf256d2c46a20ce2cf7d7fabb88b92b
Omar Khalifa <elkhalifa002@gmail.com>
2023-07-27 06:25:55 +0300

    remove shadow and unused imports

--- src/canvas/index.jsx
+++ src/canvas/index.jsx
@@ -1,13 +1,11 @@
 import { Canvas } from '@react-three/fiber'
-import { Environment, Center } from '@react-three/drei';
-
+import { Center } from '@react-three/drei';
 import Shirt from './Shirt';
 import CameraRig from './CameraRig';
 
 const CanvasModel = () => {
   return (
     <Canvas
-      shadows
       camera={{ position: [0, 0, 0], fov: 25 }}
       gl={{ preserveDrawingBuffer: true }}
       className="w-full max-w-full h-full transition-all ease-in"
